3de8dcaa335ef16215589e7b51c89026b961e331
Etheldra Akahoho <etheldeak4@gmail.com>
2024-11-22 08:44:23 +0000

    redirects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaUser, FaList, FaEnvelope, FaMap, FaCreditCard, FaShoppingCart } from 'react-icons/fa';
-import agrilogo from "../assets/images/agri.svg"
+import agrilogo from "../assets/images/agriii.jpg"
 
 const Navbar = ({ cartItemCount }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
